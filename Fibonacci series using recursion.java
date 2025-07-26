6b27a617cb25dcb93b7b044afe2ca22edd306c57
Kommalapatiabhinayasri <151392932+Kommalapatiabhinayasri@users.noreply.github.com>
2025-07-26 12:12:43 +0530

    Add files via upload

--- Fibonacci series using recursion.java
+++ Fibonacci series using recursion.java
@@ -1,12 +1,11 @@
 public class Main
 {
   public static void main(String[] args){
-    System.out.println(fib(6));
+    fib(0,1,6);
   }
-  static int fib(int n){
-    if(n==0)return 0;
-    if(n<2) return 1;
-    return fib(n-1)+fib(n-2);
-    
+  static void fib(int a,int b,int n){
+    if(n==0)return;
+    System.out.println(a+" ");
+    fib(b,a+b,n-1);
   }
 }
